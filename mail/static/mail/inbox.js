fb652c7136e8beccd057ca86a22ae890b38bc44b
Alejandro Campos <acamposcar@gmail.com>
2022-03-04 14:09:45 +0100

    Add alert to show fetch errors, fix email send and other bug fixes

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -3,6 +3,9 @@ SINGLE EMAILS VIEW
  -----------------------*/
 
 function readEmail(emailID) {
+  /*----------------
+  Mark email as read when it is opened
+  ----------------*/
   fetch(`/emails/${emailID}`, {
     method: 'PUT',
     body: JSON.stringify({
@@ -11,56 +14,76 @@ function readEmail(emailID) {
   });
 }
 
+function getEmail(emailID) {
+  /*----------------
+  Gets email information from database
+  ----------------*/
+  fetch(`/emails/${emailID}`)
+    .then((response) => response.json())
+    .then((email) => {
+      if (email.error) {
+        document.querySelector('#alert').style.display = 'block';
+        document.querySelector('#alert').textContent = email.error;
+      } else {
+        openEmail(email);
+      }
+    });
+}
+
 function openEmail(email) {
+  /*----------------
+  Creates single email view
+  ----------------*/
+
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'none';
   document.querySelector('#open-email-view').style.display = 'block';
+  document.querySelector('#alert').style.display = 'none';
 
   // Clear previous emails
   const emailContainer = document.querySelector('#open-email-view');
   emailContainer.innerHTML = '';
 
-  const recipients = document.createElement('div');
-  recipients.classList.add('open-email-recipients');
-  recipients.textContent = email.recipients;
-  emailContainer.appendChild(recipients);
-
-  const sender = document.createElement('div');
-  sender.classList.add('open-email-sender');
-  sender.textContent = email.sender;
-  emailContainer.appendChild(sender);
-
-  const time = document.createElement('div');
-  time.classList.add('open-email-time');
-  time.textContent = email.timestamp;
-  emailContainer.appendChild(time);
-
-  const subject = document.createElement('div');
+  // Create subject div
+  const subject = document.createElement('h4');
   subject.classList.add('open-email-subject');
-  subject.textContent = email.subject;
+  subject.innerHTML = email.subject;
   emailContainer.appendChild(subject);
 
+  // Create senders div
+  const sender = document.createElement('div');
+  sender.classList.add('open-email-sender');
+  sender.innerHTML = `<strong>From:</strong> ${email.sender}`;
+  emailContainer.appendChild(sender);
+
+  // Create recipients div
+  const recipients = document.createElement('div');
+  recipients.classList.add('open-email-recipients');
+  recipients.innerHTML = `<strong>To:</strong> ${email.recipients}`;
+  emailContainer.appendChild(recipients);
+
+  // Create timestamp div
+  const time = document.createElement('div');
+  time.classList.add('open-email-time');
+  time.innerHTML = `<strong>Time:</strong>: ${email.timestamp}`;
+  emailContainer.appendChild(time);
+
+  // Create reply button
   const reply = document.createElement('button');
   reply.classList.add('open-email-reply', 'btn', 'btn-primary');
   reply.textContent = 'Reply';
-  reply.addEventListener('click', () => {
-    composeEmail(email.sender, email.subject, email.body, email.timestamp);
-  });
+  reply.addEventListener('click', () => { replyEmail(email); });
   emailContainer.appendChild(reply);
 
+  // Create body div
   const body = document.createElement('div');
   body.classList.add('open-email-body');
   body.textContent = email.body;
   emailContainer.appendChild(body);
 
+  // Mark email as read when user opens the email
   readEmail(email.id);
-}
-
-function getEmail(emailID) {
-  fetch(`/emails/${emailID}`)
-    .then((response) => response.json())
-    .then((email) => openEmail(email));
 }
 
 /* --------------------
@@ -68,6 +91,9 @@ INBOX VIEW
  -----------------------*/
 
 function archiveEmail(emailID, archived) {
+  /*----------------
+  Archives or unarchives email when button is pressed
+  ----------------*/
   fetch(`/emails/${emailID}`, {
     method: 'PUT',
     body: JSON.stringify({
@@ -83,58 +109,63 @@ function archiveEmail(emailID, archived) {
 }
 
 function createEmailContainer(email, mailbox) {
+  /*----------------
+  Creates email container for every email
+  ----------------*/
+
+  // If the mail has already been read, add the class 'read'
   const emailContainer = document.createElement('div');
   if (email.read === true) {
     emailContainer.classList.add('email-container', 'read');
   } else {
-    emailContainer.classList.add('email-container');
+    emailContainer.classList.add('email-container', 'no-read');
   }
 
   // Container to define a clickable zone (All except Archive button)
   const clickableContainer = document.createElement('div');
   clickableContainer.classList.add('email-clickable-container');
+  clickableContainer.addEventListener('click', () => getEmail(email.id));
 
+  // Creates recipient/sender div
+  // If selected mailbox is inbox or archive, first field will be the sender
+  // If selected mailbox is sent, first field will be the recipient
   if (mailbox === 'inbox' || mailbox === 'archive') {
     const sender = document.createElement('div');
     sender.classList.add('email-from');
     sender.textContent = email.sender;
     clickableContainer.appendChild(sender);
   } else {
-    const receiver = document.createElement('div');
-    receiver.classList.add('email-from');
-    receiver.textContent = email.recipients[0]; // TODO: Show all recipients or more
-    clickableContainer.appendChild(receiver);
+    const recipient = document.createElement('div');
+    recipient.classList.add('email-from');
+    recipient.textContent = email.recipients[0]; // TODO: Show all recipients or more
+    clickableContainer.appendChild(recipient);
   }
 
+  // Creates subject div
   const subject = document.createElement('div');
   subject.classList.add('email-subject');
   subject.textContent = email.subject.slice(0, 50);
   clickableContainer.appendChild(subject);
 
+  // Creates timestamp div
   const time = document.createElement('div');
   time.classList.add('email-time');
   time.textContent = email.timestamp;
   clickableContainer.appendChild(time);
 
+  // Appends Clickable container to email container
   emailContainer.appendChild(clickableContainer);
 
+  // Add archive/unarchive button to inbox and archive mailboxes
   if (mailbox !== 'sent') {
     const archiveButton = document.createElement('button');
     archiveButton.classList.add('email-archive', 'btn', 'btn-primary');
-    if (email.archived) {
-      archiveButton.textContent = 'Unarchive';
-    } else {
-      archiveButton.textContent = 'Archive';
-    }
-    archiveButton.addEventListener('click', () => {
-      archiveEmail(email.id, email.archived);
-    });
+    archiveButton.textContent = email.archived ? 'Unarchive' : 'Archive';
+    archiveButton.addEventListener('click', () => { archiveEmail(email.id, email.archived); });
     emailContainer.appendChild(archiveButton);
   }
 
   document.querySelector('#emails-view').appendChild(emailContainer);
-
-  clickableContainer.addEventListener('click', () => getEmail(email.id));
 }
 
 /* --------------------
@@ -142,6 +173,9 @@ SEND EMAILS
  -----------------------*/
 
 function sendEmail() {
+  /*----------------
+  Send email (POST)
+  ----------------*/
   fetch('/emails', {
     method: 'POST',
     body: JSON.stringify({
@@ -151,45 +185,51 @@ function sendEmail() {
     }),
   })
     .then((response) => response.json())
-    .catch((error) => console.error('Error:', error)) // TODO handle error. Call another function???
-    .then((result) => console.log('Success:', result))
-    .then(loadMailbox('sent'));
+    .then((result) => {
+      if (result.error) {
+        document.querySelector('#alert').style.display = 'block';
+        document.querySelector('#alert').textContent = result.error;
+      } else {
+        loadMailbox('sent');
+      }
+    });
 }
 
-function composeEmail(recipient = '', subject = '', body = '', time = '') {
+function composeEmail() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
   document.querySelector('#open-email-view').style.display = 'none';
-
-  let replySubject;
-  if (subject.slice(0, 3) !== 'Re:' && recipient !== '') {
-    replySubject = `Re: ${subject}`;
-  } else {
-    replySubject = subject;
-  }
-
-  let replyBody;
-  if (recipient !== '') {
-    replyBody = `
-
------------------------------------
-On ${time} ${recipient} wrote:
-${body}`;
-  } else {
-    replyBody = body;
-  }
+  document.querySelector('#alert').style.display = 'none';
 
   // Clear out composition fields
-  document.querySelector('#compose-recipients').value = recipient;
-  document.querySelector('#compose-subject').value = replySubject;
-  document.querySelector('#compose-body').value = replyBody;
-  document
-    .querySelector('input[type="submit"]')
-    .addEventListener('click', (event) => {
-      event.preventDefault(); // If not Submit button will make GET request
-      sendEmail();
-    });
+  document.querySelector('#compose-recipients').value = '';
+  document.querySelector('#compose-subject').value = '';
+  document.querySelector('#compose-body').value = '';
+
+  document.querySelector('form').onsubmit = () => {
+    sendEmail();
+    return false;
+  };
+}
+
+function replyEmail(email) {
+  /*----------------
+  Pre-fill form to reply to emails
+  ---------------- */
+
+  composeEmail();
+
+  document.querySelector('#compose-recipients').value = email.sender;
+
+  // Add Re: to reply subject if it isn't there already
+  if (email.subject.slice(0, 3) !== 'Re:') {
+    document.querySelector('#compose-subject').value = `Re: ${email.subject}`;
+  } else {
+    document.querySelector('#compose-subject').value = email.subject;
+  }
+
+  document.querySelector('#compose-body').value = `\n---------\nOn ${email.timestamp} ${email.sender} wrote:\n${email.body}`;
 }
 
 /* --------------------
@@ -201,33 +241,29 @@ function loadMailbox(mailbox) {
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
   document.querySelector('#open-email-view').style.display = 'none';
+  document.querySelector('#alert').style.display = 'none';
 
   // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${
-    mailbox.charAt(0).toUpperCase() + mailbox.slice(1)
-  }</h3>`;
+  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
   fetch(`/emails/${mailbox}`)
     .then((response) => response.json())
     .then((emails) => {
-      emails.forEach((email) => createEmailContainer(email, mailbox));
+      if (emails.error) {
+        document.querySelector('#alert').style.display = 'block';
+        document.querySelector('#alert').textContent = emails.error;
+      } else {
+        emails.forEach((email) => createEmailContainer(email, mailbox));
+      }
     });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   // Use buttons to toggle between views
-  document
-    .querySelector('#inbox')
-    .addEventListener('click', () => loadMailbox('inbox'));
-  document
-    .querySelector('#sent')
-    .addEventListener('click', () => loadMailbox('sent'));
-  document
-    .querySelector('#archived')
-    .addEventListener('click', () => loadMailbox('archive'));
-  document.querySelector('#compose').addEventListener('click', () => {
-    composeEmail();
-  });
+  document.querySelector('#inbox').addEventListener('click', () => loadMailbox('inbox'));
+  document.querySelector('#sent').addEventListener('click', () => loadMailbox('sent'));
+  document.querySelector('#archived').addEventListener('click', () => loadMailbox('archive'));
+  document.querySelector('#compose').addEventListener('click', composeEmail);
   // By default, load the inbox
   loadMailbox('inbox');
 });
